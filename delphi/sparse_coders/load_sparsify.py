a4b36adacc01fdae67ae01b0e3030080687aabf1
ale_9702 <alessandroruffolo29@gmail.com>
2025-09-11 15:59:11 +0200

    Final code

--- delphi/sparse_coders/load_sparsify.py
+++ delphi/sparse_coders/load_sparsify.py
@@ -82,7 +82,7 @@ def load_sparsify_sparse_coders(
                 )
     else:
         # Load on CPU first to not run out of memory
-        sparse_models = SparseCoder.load_many(name, device="cpu", local=False, layers=hookpoints)
+        sparse_models = SparseCoder.load_many(name, local=False, layers=hookpoints, device="cpu")
         for hookpoint in hookpoints:
             sparse_model_dict[hookpoint] = sparse_models[hookpoint].to(device)
             if compile:
